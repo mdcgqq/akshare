c90c946aba8b628a3f76916a8d6874b8abd0fc27
成宇 <lihuaqi@xiaohongshu.com>
2025-01-18 17:56:50 +0800

    add: 添加短线王涨停板接口

---
+++ akshare/stock_feature/stock_ztb_dxw.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Date: 2025/01/18 17:38
+Desc: 首页-打板神器
+https://api.duishu.com/lhbapp/zhangting/newDaban
+
+"""
+
+from datetime import datetime, timedelta
+
+import pandas as pd
+import requests
+
+def _dxwsign(values:dict):
+    pass
